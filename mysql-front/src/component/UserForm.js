29633914802b02d769df797ee531fb5d7f881230
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-07-29 11:24:40 +0900

    naverLogin:

--- mysql-front/src/component/UserForm.js
+++ mysql-front/src/component/UserForm.js
@@ -31,7 +31,8 @@ const UserForm = ({ addUser }) => {
             required
           />
         </div>
-        <button type="submit">유저 추가하기</button>
+        <button type="submit">유저 추가하기</button><br/><br/><br/>
+        <button type="submit">네이버 로그인을 통한 유저 추가하기</button>
       </form>
     </div>
   );
