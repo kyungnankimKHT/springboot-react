e89df7ffd33a06f16b13ec7dc9b315e8d96bf0cd
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-08-06 09:51:52 +0900

    image upload

--- spring-image-back/src/main/java/com/kh/service/PostServiceImpl.java
+++ spring-image-back/src/main/java/com/kh/service/PostServiceImpl.java
@@ -3,6 +3,7 @@ package com.kh.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.kh.dto.Post;
@@ -14,23 +15,22 @@ public class PostServiceImpl implements PostService{
 	@Autowired
 	private PostMapper postMapper;
 	
-	@Override
-	public List<Post> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	//import org.springframework.beans.factory.annotation.Value;
+	@Value("${file.upload-dir}") //application.properties에서 설정 이름 가져와
+	private String uploadDir; 
+	//private String uploadDir = "C:/Users/user1/Desktop/saveImage";
+	//file.upload-dir=C:/Users/user1/Desktop/saveImage 경로 넣어서 사용
+	
 	
 	@Override
-	public void insertImage() {
-		// TODO Auto-generated method stub
-		
+	public List<Post> findAll() {
+		return postMapper.findAll();
 	}
 	
 	
 	@Override
 	public void insertPost(Post post) {
-		// TODO Auto-generated method stub
-		
+		postMapper.insertPost(post);	
 	}
 	
 	
