cd287d7b86b693d333d34ed8de23e8d5e52843a8
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-08-14 10:43:02 +0900

    bycrto

--- spring-password/src/main/java/com/kh/dto/BCUser.java
+++ spring-password/src/main/java/com/kh/dto/BCUser.java
@@ -4,13 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
 @Entity //mysql 매핑
 public class BCUser {
 	
 	@Id // primary key
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private String id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 자동으로 id값 올라가는 시퀀스 NextVal
+	private int id;
 	private String username;
 	private String email;
 	private String password;
