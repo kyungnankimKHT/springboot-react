2aa28fce3deaf888871aba4462ae0abe067402f6
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-08-19 11:27:55 +0900

    api

--- front-xml-api/src/App.js
+++ front-xml-api/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import AirPollutionData from './AirPollutionData';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <h1>공기오염 조회서비스</h1>
-      
+      <AirPollutionData />
       </header>
     </div>
   );
