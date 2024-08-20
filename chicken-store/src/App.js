d7afd8f000be7b94f1fef1871be18bfaebdee399
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-08-20 12:40:31 +0900

    .

--- chicken-store/src/App.js
+++ chicken-store/src/App.js
@@ -1,15 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import ChickenList from './component/ChickenList';
-import ChickenForm from './component/ChickenForm';
+import logo from "./logo.svg";
+import "./App.css";
+import ChickenList from "./component/ChickenList";
+import ChickenForm from "./component/ChickenForm";
+import Modal from "./component/Modal";
+import { useState } from "react";
+const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false); 
+  //사용자가 보고싶을 때 볼 수 있도록 처음에는 false 보이지않음 설정 해줌
 
-function App() {
+  // 오픈 true 닫음 false
+  // const에서 동작하는 기능이 1개일 때 {} 중괄호 생략 가능
+  const openModal = () => setIsModalOpen(true);
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  }
+
   return (
-    <div className="App">
-     <ChickenList />
-     <ChickenForm />
+    <div className="app-container">
+      <h1>치킨 가게 메뉴 관리</h1>
+      <ChickenList />
+
+
+      <button onClick={openModal}>메뉴등록하기</button>
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+          <ChickenForm />
+      </Modal>
     </div>
   );
-}
+};
 
 export default App;
--- chicken-store/src/component/ChickenList.js
+++ chicken-store/src/component/ChickenList.js
@@ -2,13 +2,16 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 const ChickenList = () => {
-    const [ chickens, setChickens] = useState([]);
+    const [chickens, setChickens] = useState([]);
 
 // 최초 1회 실행 useEffect 이용해서 처음에 치킨리스트.js 시작하자마자 DB에 저장된 치킨메뉴들 가져오기
 //useEffect(() => {기능설정}, [언제 다시 기능을 동작 시킬 것인가])
 useEffect(() => {
     axios.get("http://localhost:9090/api/chicken")
-    .then(response => setChickens(response.data)) //가져온 데이터를 chickens 변수에 저장하는 과정
+    .then(response => {
+        setChickens(response.data);
+        console.log("chickens : " , chickens);
+    }) //가져온 데이터를 chickens 변수에 저장하는 과정
     .catch(e => alert("불러오는데 문제가 발생했습니다."));
 }, [])
 
@@ -18,7 +21,8 @@ useEffect(() => {
             <ul>
             {chickens.map(chicken => (
                 <li key={chicken.id}>
-                    {chicken.name} = {chicken.description} = ₩{chicken.price}원
+                    {chicken.chickenName}  {chicken.description}  ₩{chicken.price}원
+                   
                 </li>
             ))}
             </ul>
