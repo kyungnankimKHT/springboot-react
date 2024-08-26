909cb0910ac6018815736dabbf8d9be0fe4aac1d
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-08-26 12:10:31 +0900

    pizza search

--- pizza-store/src/components/PizzaForm.js
+++ pizza-store/src/components/PizzaForm.js
@@ -13,21 +13,27 @@ const PizzaForm = () => {
     }
     // 스프링부트 연결 후 input에 작성한 데이터 전달
     const handleRegister = () => {
-        axios    then   catch
+        axios.post('http://localhost:9090/api/pizza', 전달데이터)
+        .then((response) => {
+            alert("메뉴가 성공적으로 등록되었습니다.")   
+        })   
+        .catch((e) => {
+            alert("메뉴 등록에 실패했습니다.")
+        });
     }
     return (
         <div className="pizzaform-container">
             <label>
                 메뉴 이름 : 
-                <input value={} onChange={}/>
+                <input value={pizzaName} onChange={(e) =>setPizzaName(e.target.value)}/>
             </label>
             <label>
                 메뉴 설명 : 
-                <input  value={} onChange={}/>
+                <input  value={description} onChange={(e) => setDescription(e.target.value)}/>
             </label>
             <label>
                 메뉴 가격 : 
-                <input  value={} onChange={}/>
+                <input  value={price} onChange={(e) => setPrice(e.target.value)}/>
             </label>
             <button onClick={handleRegister}>등록하기</button>
         </div>
